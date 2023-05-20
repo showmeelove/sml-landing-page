ebcf5288243f75b004b9b3555ed056d5c7da996b
Victor Olatunji <victoluolatunji@gmail.com>
2023-05-20 03:14:22 +0100

    lets get it guys

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,64 @@
+import Form from "./components/form";
 
 const App = () => {
   return (
     <div className="mx-auto container">
-      <div className="gradient"></div>
-      <div className="mt-[66.5px] mx-[100px] mxauto flex justify-between items-center">
+      <div className="hero-pattern"></div>
+      {/* <div className="gradient"></div> */}
+      <div className="mt-[66.5px] mx-[100px] m-auto flex justify-between items-center">
         <h1 className="text-[#FEFEFE] font-Eina01Bold text-[26px] leading-[30.47px] ">
           showmeelove.
         </h1>
         <nav>
           <ul className="flex justify-between space-x-[43px] items-center">
             <li>
-              <a href="#">features</a>
+              <a
+                className="text-[20px] font-medium font-figtree leading-[33px] "
+                href="#"
+              >
+                features
+              </a>
             </li>
             <li>
-              <a href="#">faqs</a>
+              <a
+                className="text-[20px] font-medium font-figtree leading-[33px] "
+                href="#"
+              >
+                faqs
+              </a>
             </li>
-            <button className="bg-gradient-to-r from-[#1864FF] to-[#42AAFF]">
+            <button className="h-[69px] w-[200px] font-figtree text-[18px] leading-[33pxx] text-center rounded-[40px] bg-gradient-to-r from-[#1864FF] to-[#42AAFF]">
               get early access
             </button>
           </ul>
         </nav>
       </div>
+      <div>
+        <h3 className="mx-auto mt-[77px] text-[64px] leading-[70px] text-center max-w-[901px] font-Eina01Bold  ">
+          let your fans{" "}
+          <span className="bg-clip-text text-transparent text-[43px] leading-[49px] md:text-[64px] md:leading-[70px] font-Eina01Bold lowercase bg-gradient-to-r from-[#1864FF] to-[#42AAFF]">
+            show you love
+          </span>{" "}
+          for your{" "}
+          <span className="bg-clip-text text-transparent text-[43px] leading-[49px] md:text-[64px] md:leading-[70px] font-Eina01Bold lowercase bg-gradient-to-r from-[#1864FF] to-[#42AAFF]">
+            contents
+          </span>
+        </h3>
+      </div>
+      <div>
+        <p className="mx-auto max-w-[700px] text-center mt-[41px] text-[20px] leading-[33px] font-figtree ">
+          got a devoted fanbase? <br /> you can quickly create and setup your
+          page to let your fans{" "}
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#1864FF] to-[#42AAFF]">
+            show you love
+          </span>{" "}
+          through donations, membership plans or selling anything you like.
+        </p>
+      </div>
+      {/* form */}
+      <Form />
     </div>
   );
-}
+};
 
-export default App
+export default App;
---
+++ src/components/form.jsx
@@ -0,0 +1,17 @@
+
+const form = () => {
+  return (
+    <div className="flex justify-center items-center container">
+      <form className="mx-auto mt-[68px]" action="#">
+        <input
+          type="mail"
+          name="create your page"
+          placeholder="showmeelove.com/yourname"
+          className="mx-auto bg-[#01193C] rounded-[60px] pl-[44px] min-w-[634px] py-[32px] text-[#FFFFFF] "
+        />
+      </form>
+    </div>
+  );
+};
+
+export default form;
